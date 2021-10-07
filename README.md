fc52ac1a8fee9777859cb5c4aafc8e65ba3b53d6
jeptun <jeptun@gmail.com>
2021-10-07 18:17:23 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,68 @@
+<div id="top"></div>
+
+
+<!-- PROJECT LOGO -->
+<br />
+<div align="center">
+  <a href="https://reactjs.org/">
+    <img src="images/logo.svg" alt="Logo" width="80" height="80">
+  </a>
+
+  <h3 align="center">React-Tunes</h3>
+
+  <p align="center">
+    Tailwindcss zkouška. Nedodělané!
+    <br />
+    <br />
+    .
+    <a href="https://obkladyzlin.netlify.app/">View Demo</a>
+    .
+  </p>
+</div>
+
+<!-- ABOUT THE PROJECT -->
+## O projektu
+
+[![Product Name Screen Shot][product-screenshot]](https://obkladyzlin.netlify.app/)
+
+Testování Tailwindu a různých knihoven jako Gsap k animaci obrázků a Swiper na carousel. 
+
+<p align="right">(<a href="#top">back to top</a>)</p>
+
+
+
+### Frameworks/Libraries
+
+V aplikaci je použito.
+
+* [Tailwindcss](https://tailwindcss.com/docs)
+* [Gsap](https://greensock.com/gsap/)
+* [Swiper](https://swiperjs.com//)
+
+<p align="right">(<a href="#top">back to top</a>)</p>
+
+
+<!-- GETTING STARTED -->
+
+### Instalace
+
+1. Install packages
+```sh
+  npm install 
+  ```
+2. Runs the app in the development mode Open [http://localhost:3000/](http://localhost:3000)
+
+
+<p align="right">(<a href="#top">back to top</a>)</p>
+
+<!-- CONTACT -->
+## Contact
+
+Josef Dosoudil  - jeptun@gmail.com
+
+<p align="right">(<a href="#top">back to top</a>)</p>
+
+
+<!-- MARKDOWN LINKS & IMAGES -->
+
+[product-screenshot]: images/img2.png
---
+++ tailwind.config.js
@@ -0,0 +1,59 @@
+module.exports = {
+  purge: {
+    enabled: true,
+    content: [
+      "./public/*.html"
+    ]
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        teal: {
+          50: "#F0FDFA",
+          100: "#CCFBF1",
+          200: "#99F6E4",
+          300: "#5EEAD4",
+          400: "#2DD4BF",
+          500: "#14B8A6",
+          600: "#0D9488",
+          700: "#0F766E",
+          800: "#115E59",
+          900: "#134E4A",
+        },
+        cyan: {
+          50: "#ECFEFF",
+          100: "#CFFAFE",
+          200: "#A5F3FC",
+          300: "#67E8F9",
+          400: "#22D3EE",
+          500: "#06B6D4",
+          600: "#0891B2",
+          700: "#0E7490",
+          800: "#155E75",
+          900: "#164E63",
+        },
+      },
+
+      screens: {
+        
+      },
+      fontFamily: {
+        galanoGrotesqueBold: ['"GalanoGrotesqueBold"'],
+        galanoGrotesqueThin: ['"GalanoGrotesqueThin"'],
+        galanoGrotesqueLight: ['"GalanoGrotesqueLight"'],
+        galanoGrotesqueMedium: ['"GalanoGrotesqueMedium"'],
+        galanoGrotesqueRegular: ['"GalanoGrotesqueRegular"'],
+        galanoGrotesqueSemiBold: ['"GalanoGrotesqueSemiBold"'],
+      },
+    },
+  },
+  variants: {
+    extend: {
+      backgroundColor: ['active'],
+      grayscale: ['hover', 'focus'],
+      scale: ['active', 'group-hover'],
+    },
+  },
+  plugins: [],
+};
